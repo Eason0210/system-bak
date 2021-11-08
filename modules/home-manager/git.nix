65e0eb69c6898c08254a1180d44b9d112a7f754f
Eason0210 <aqua0210@163.com>
2021-11-09 00:17:27 +0800

    adjust git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,22 +1,30 @@
 { config, lib, pkgs, ... }: {
   home.packages = [ pkgs.github-cli ];
   programs.git = {
-    userName = "Kennan LeJeune";
+    userName = "Eason Huang";
+    userEmail = "aqua0210@163.com";
     extraConfig = {
-      credential.helper = if pkgs.stdenvNoCC.isDarwin then
-        "osxkeychain"
-      else
-        "cache --timeout=1000000000";
+      credential.helper =
+        if pkgs.stdenvNoCC.isDarwin then
+          "osxkeychain"
+        else
+          "cache --timeout=1000000000";
       http.sslVerify = true;
       pull.rebase = false;
       commit.verbose = true;
-      init.defaultBranch = "main";
+      init.defaultBranch = "master";
+      hub.protocol = "https";
+      github.user = "Eason0210";
+      color.ui = true;
+      merge.conflictstyle = "diff3";
+      diff.algorithm = "patience";
+      protocol.version = "2";
+      core.commitGraph = true;
+      gc.writeCommitGraph = true;
     };
     aliases = {
       fix = "commit --amend --no-edit";
       oops = "reset HEAD~1";
     };
-    delta.enable = true;
-    lfs.enable = true;
   };
 }
